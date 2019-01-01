5fcc26497ab2c713d8e57fd599033bfbad25fca6
Bill Zhang <billzzhang2@gmail.com>
2019-01-01 11:29:10 -0500

    changed database relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_195839) do
+ActiveRecord::Schema.define(version: 2019_01_01_162818) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_categories_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "question"
-    t.string "correct_answer"
-    t.text "incorrect_answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
