e49f18f5ee214fbbb53fd0f49eeb007185aab963
Bill Zhang <billzzhang2@gmail.com>
2019-01-05 16:04:41 -0500

    added new page: play

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,7 @@ class QuestionsController < ApplicationController
     def show
         @question = Question.find(params[:id])
     end
+    def play
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :difficulty
   get 'questions' => 'questions#index'
   get 'questions/:id' => 'questions#show'
+  get 'play' => 'questions#play'
 end
