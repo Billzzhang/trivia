7650582515fcaa49bed94fcb36634465fab8ee7d
Bill Zhang <billzzhang2@gmail.com>
2019-01-06 22:16:28 -0500

    added more dynamic category inclusion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-l = Difficulty.create(name: "easy")
-mandt = Category.create(name: "Entertainment: Musicals & Theatre") 
-l.questions.create(question: "When was Macbeth written", difficulty_id:1, category_id: 1)
+
+#l = Difficulty.create(name: "easy")
+#mandt = Category.create(name: "Entertainment: Musicals & Theatre") 
+#l.questions.create(question: "When was Macbeth written", difficulty_id:1, category_id: 1)
+require 'json'
+require 'set'
+Category.destroy_all
+Category.reset_pk_sequence
+open("trivia.json") do |categories|
+    data = []
+    categories.read.each_line do |c|
+        @item = JSON.parse(c)
+        @item["results"].each do |a|
+            object = {
+                "name": a["category"]
+            }
+            data << object unless data.include? object
+        end
+    end
+    Category.create!(data)
+end
